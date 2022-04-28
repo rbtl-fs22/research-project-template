48740c0ef2b295ca0a053da3491e8f92f0480721
Lars Schöbitz <lars@lse.de>
2022-04-28 12:40:44 +0200

    Add description

---
+++ src/01-tidy-raw-data.R
@@ -0,0 +1,11 @@
+# description -------------------------------------------------------------
+
+# This is an R file. It will be edited by the course coordinators. As an R
+# file it only contains R code. All text is added as comments starting with 
+# the hash # sign at the beginning of the line. It is one big code-chunk if
+# you imagine an R Markdown or Quarto file that combined code and text.
+
+# -------------------------------------------------------------------------
+
+
+
